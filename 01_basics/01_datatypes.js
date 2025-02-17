0da3db59e440dbfb906c2d0c99613ad9ab144c5b
Shree4226 <shreesim4226@gmail.com>
2025-02-17 10:28:43 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we're using nodejs , not browser
+
+// code readability should be high
+
+
+let name="Shreya"
+let age=20
+let isEmpty=false
+let state
+
+// number => 2 to the power 53
+// bigInt
+// strings
+// boolean
+// null: standalone value
+// undefined
+// symbol=> unique
+
+// objects
+
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+
+
